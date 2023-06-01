87ea7de6dfc483108431bf292a078e6d5c3a91c2
bkaviyukan <135192928+bkaviyukan@users.noreply.github.com>
2023-06-01 11:58:56 +0530

    Add files via upload

---
+++ pp3 project 1.py
@@ -0,0 +1,22 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/NEXT GEN/AppData/Local/Programs/Python/Python311/pp3 project 1.py
+Traceback (most recent call last):
+  File "C:/Users/NEXT GEN/AppData/Local/Programs/Python/Python311/pp3 project 1.py", line 1, in <module>
+    Print ('Hello, World!')
+NameError: name 'Print' is not defined. Did you mean: 'print'?
+>>> 
+= RESTART: C:/Users/NEXT GEN/AppData/Local/Programs/Python/Python311/pp3 project 1.py
+Traceback (most recent call last):
+  File "C:/Users/NEXT GEN/AppData/Local/Programs/Python/Python311/pp3 project 1.py", line 1, in <module>
+    Print ('Hello, World!')
+NameError: name 'Print' is not defined. Did you mean: 'print'?
+>>> 
+= RESTART: C:/Users/NEXT GEN/AppData/Local/Programs/Python/Python311/pp3 project 1.py
+Hello, World!
+>>> 
+= RESTART: C:/Users/NEXT GEN/AppData/Local/Programs/Python/Python311/pp3 project 1.py
+Hello, World!
+what is your name? Kaviyukan
+Hello  Kaviyukan
