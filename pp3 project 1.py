c6db36f2efa4577566e350e1636f8f9b913fde28
bkaviyukan <135192928+bkaviyukan@users.noreply.github.com>
2023-06-08 11:33:40 +0530

    Add files via upload

--- pp3 project 1.py
+++ pp3 project 1.py
@@ -1,33 +1,3 @@
-Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-= RESTART: C:/Users/NEXT GEN/AppData/Local/Programs/Python/Python311/pp3 project 1.py
-Traceback (most recent call last):
-  File "C:/Users/NEXT GEN/AppData/Local/Programs/Python/Python311/pp3 project 1.py", line 1, in <module>
-    Print ('Hello, World!')
-NameError: name 'Print' is not defined. Did you mean: 'print'?
->>> 
-= RESTART: C:/Users/NEXT GEN/AppData/Local/Programs/Python/Python311/pp3 project 1.py
-Traceback (most recent call last):
-  File "C:/Users/NEXT GEN/AppData/Local/Programs/Python/Python311/pp3 project 1.py", line 1, in <module>
-    Print ('Hello, World!')
-NameError: name 'Print' is not defined. Did you mean: 'print'?
->>> 
-= RESTART: C:/Users/NEXT GEN/AppData/Local/Programs/Python/Python311/pp3 project 1.py
-Hello, World!
->>> 
-= RESTART: C:/Users/NEXT GEN/AppData/Local/Programs/Python/Python311/pp3 project 1.py
-Hello, World!
-what is your name? Kaviyukan
-Hello  Kaviyukan
->>> age=13
->>> print(age)
-13
->>> weight=45
->>> print (weight)
-45
->>> height=129cm
-SyntaxError: invalid decimal literal
->>> height=100
->>> print (height)
-100
+print('Hello, world!')
+person=input('what is your name?')
+print('Hello',person)
