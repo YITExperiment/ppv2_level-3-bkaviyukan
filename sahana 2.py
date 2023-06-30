43e9de604ddd904bfea0b8987930fb4cbec98c07
bkaviyukan <135192928+bkaviyukan@users.noreply.github.com>
2023-06-30 12:43:21 +0530

    Add files via upload

---
+++ sahana 2.py
@@ -0,0 +1,26 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/User/AppData/Local/Programs/Python/Python311/sahana.py
+Traceback (most recent call last):
+  File "C:/Users/User/AppData/Local/Programs/Python/Python311/sahana.py", line 1, in <module>
+    print ['Hello, world!']
+TypeError: 'builtin_function_or_method' object is not subscriptable
+>>> 
+=== RESTART: C:/Users/User/AppData/Local/Programs/Python/Python311/sahana.py ===
+Hello, world!
+>>> 
+=== RESTART: C:/Users/User/AppData/Local/Programs/Python/Python311/sahana.py ===
+Hello, world!
+what is your name?sahana
+hello sahana
+>>> 
+>>> age=12
+>>> print(age)
+12
+>>> weight=42
+>>> print(weight)
+42
+>>> height=154
+>>> print(height)
+154
